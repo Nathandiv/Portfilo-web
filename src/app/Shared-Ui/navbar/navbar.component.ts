33cdd22d3b5a9580b9de2d04fec9a244c9e44104
Nathandiv <k.n.hlungwani@gmail.com>
2025-02-05 12:36:58 +0200

    made changes on navbar

--- src/app/Shared-Ui/navbar/navbar.component.ts
+++ src/app/Shared-Ui/navbar/navbar.component.ts
@@ -8,13 +8,5 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  // ViewChild to reference mobile menu
-  @ViewChild('mobileMenu') mobileMenu!: ElementRef;
 
-  // Method to toggle the hidden class on the mobile menu
-  toggleMobileMenu(): void {
-    if (this.mobileMenu) {
-      this.mobileMenu.nativeElement.classList.toggle('hidden');
-    }
-  }
 }

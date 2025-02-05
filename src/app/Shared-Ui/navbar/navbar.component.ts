c65ad47e849fc79cc9aa8b7a78416987b9cc5a9f
Nathandiv <k.n.hlungwani@gmail.com>
2025-02-05 11:38:14 +0200

    added navbar and styled it also added it to home page

--- src/app/Shared-Ui/navbar/navbar.component.ts
+++ src/app/Shared-Ui/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  // ViewChild to reference mobile menu
+  @ViewChild('mobileMenu') mobileMenu!: ElementRef;
 
+  // Method to toggle the hidden class on the mobile menu
+  toggleMobileMenu(): void {
+    if (this.mobileMenu) {
+      this.mobileMenu.nativeElement.classList.toggle('hidden');
+    }
+  }
 }

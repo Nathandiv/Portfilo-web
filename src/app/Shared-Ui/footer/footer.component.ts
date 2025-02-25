19c17f5c1c48e82a20f34cb98cff5cf1a5839a11
Nathandiv <k.n.hlungwani@gmail.com>
2025-02-25 15:52:18 +0200

    done styling the footer

--- src/app/Shared-Ui/footer/footer.component.ts
+++ src/app/Shared-Ui/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular'; // Import IonicModule for ion-icon
+import { RouterModule } from '@angular/router'; // Import RouterModule for routerLink
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [IonicModule, RouterModule], // Add IonicModule and RouterModule to imports
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../Shared-Ui/navbar/navbar.component";
+import { FooterComponent } from "../../Shared-Ui/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

e1a5256a8556b26c1f5fe9482578b380e9d72d67
Nathandiv <k.n.hlungwani@gmail.com>
2025-03-20 15:52:46 +0200

    added animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'Nath-App';
 
   ngOnInit(): void {
     initFlowbite();
+    AOS.init();
   }
-
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from "../../Shared-Ui/navbar/navbar.component";
 import { FooterComponent } from "../../Shared-Ui/footer/footer.component";
+import AOS from 'aos';
+import Typed from 'typed.js';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +12,30 @@ import { FooterComponent } from "../../Shared-Ui/footer/footer.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    // Initialize AOS
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
 
+    // Initialize Typed.js
+    const typed = new Typed('.typed', {
+      strings: [
+        "System Developement",
+        "Full Stack Developer",
+        "Web Developer",
+        "Mobile Developer",
+        "Software Developer",
+      ],
+      typeSpeed: 80,
+      backSpeed: 70,
+      backDelay: 1500,
+      loop: true,
+      startDelay: 1000,
+    });
+  }
 }

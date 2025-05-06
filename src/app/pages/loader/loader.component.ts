a009b1ae25651fcd23df6363388369ab45fd09d6
Nathandiv <k.n.hlungwani@gmail.com>
2025-05-06 09:19:26 +0200

    add input property to loader component for loading state

--- src/app/pages/loader/loader.component.ts
+++ src/app/pages/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './loader.component.css'
 })
 export class LoaderComponent {
+
+  @Input() isLoading: boolean = false
+
 
 }
